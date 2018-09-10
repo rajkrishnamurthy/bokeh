9d1308fab28f2e72e0de2a72020dda077707fcd0
Mateusz Paprocki <mattpap@gmail.com>
2018-09-10 17:35:04 +0200

    Rebuild layout if children change

--- bokehjs/src/lib/models/layouts/layout_dom.ts
+++ bokehjs/src/lib/models/layouts/layout_dom.ts
@@ -36,11 +36,14 @@ export abstract class LayoutDOMView extends DOMView {
   connect_signals(): void {
     super.connect_signals()
 
-    if (this.is_root)
-      window.addEventListener("resize", () => this.root.do_layout())
+    if (this.is_root) {
+      window.addEventListener("resize", () => this.compute_layout())
+    }
 
-    // XXX: this.connect(this.model.change, () => this.root.do_layout())
-    this.connect(this.model.properties.sizing_mode.change, () => this.root.do_layout())
+    this.connect(this.model.properties.sizing_mode.change, () => {
+      this.root.update_layout()
+      this.root.compute_layout()
+    })
   }
 
   css_classes(): string[] {
@@ -99,12 +102,25 @@ export abstract class LayoutDOMView extends DOMView {
   }
 
   renderTo(element: HTMLElement): void {
-    super.renderTo(element)
-    this.update_layout()
-    this.do_layout()
+    element.appendChild(this.el)
+    this.build()
   }
 
-  do_layout(): void {
+  build(): void {
+    this.assert_root()
+    this.render()
+    this.update_layout()
+    this.compute_layout()
+  }
+
+  rebuild(): void {
+    this.build_child_views()
+    this.render()
+    this.root.update_layout()
+    this.root.compute_layout()
+  }
+
+  compute_layout(): void {
     const start = Date.now()
     const viewport = this._viewport_size()
     this.layout.compute(viewport)
